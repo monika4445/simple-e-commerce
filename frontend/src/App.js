0db901af1bfc5d36bf1feb1d71eeff0e81e8aa62
monika4445 <monika.zaqaryan02@mail.ru>
2023-03-01 12:49:48 +0400

    added fetch  data from backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,46 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './ProductList';
 import ProductDetail from './ProductDetail';
-// import data from './utils';
 import Header from './Header';
 import Footer from './Footer';
-import ShoppingCart from "./ShoppingCart";
+import ShoppingCart from './ShoppingCart';
 
 export default function App() {
-  const [products] = useState(data);// fetch from backend api
+  const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
 
-  return ( 
-    <Fragment>
-        <Header />
-          <Router>
-            <Routes>
-                <Route path="/" element={<ProductList products={products} />} />
-                <Route path="/product/:id" 
-                       element={<ProductDetail products={products} addToCart={(product) => setCart([...cart, product])} />} />
-                <Route path="/cart" element={<ShoppingCart cart={cart} setCart={setCart} />} />
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:4000/books');
+      const data = await response.json();
+      setProducts(data);
+    };
+    fetchData();
+  }, []);
 
-            </Routes>
-          </Router>
-          <Footer />
+  return (
+    <Fragment>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProductList products={products} />} />
+          <Route
+            path="/product/:id"
+            element={
+              <ProductDetail
+                products={products}
+                addToCart={(product) => setCart([...cart, product])}
+              />
+            }
+          />
+          <Route
+            path="/cart"
+            element={<ShoppingCart cart={cart} setCart={setCart} />}
+          />
+        </Routes>
+      </Router>
+      <Footer />
     </Fragment>
   );
 }
-
